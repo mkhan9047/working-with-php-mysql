55b7f701fe5fa5010a682d4b4c9fbf818fdf5982
Mujahid <mkhan9047@gmail.com>
2018-01-13 09:15:32 +0600

    Recycler view UI added

---
+++ app/src/main/java/com/example/mujahid/working_with_php_mysql/ShowData.java
@@ -0,0 +1,15 @@
+package com.example.mujahid.working_with_php_mysql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data);
+    }
+
+
+}
